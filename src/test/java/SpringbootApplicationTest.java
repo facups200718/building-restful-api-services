9644de7c4755fc76f26ef9ff95cd98f621069ee6
Facundo Perez Schneider <facup200718@gmail.com>
2023-01-23 19:58:58 -0300

    Arreglo el test

--- src/test/java/SpringbootApplicationTest.java
+++ src/test/java/SpringbootApplicationTest.java
@@ -1,7 +1,8 @@
+import com.skillsoft.springboot.SpringbootApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootApplication.class)
 class SpringbootApplicationTest {
 
     //Ab
