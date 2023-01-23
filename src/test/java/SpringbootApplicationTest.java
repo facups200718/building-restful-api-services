f575dc252f26759286ff14a2871dcc5a9610fcee
Facundo Perez Schneider <facup200718@gmail.com>
2023-01-23 20:28:15 -0300

    a ver que pasa

--- src/test/java/SpringbootApplicationTest.java
+++ src/test/java/SpringbootApplicationTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = SpringbootApplication.class)
 class SpringbootApplicationTest {
 
-    //Ab
+    //Aver que pasa
     @Test
     void contextLoads() {
     }
